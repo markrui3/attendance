f36f4fe2124b4039aaed121aa57de1b75d8bc4c7
markrui <87541708@qq.com>
2015-12-19 02:46:40 +0800

    前端页面精细完成

---
+++ front/templates/self/events.json.php
@@ -0,0 +1,85 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "294",
+			"title": "准时！",
+			"url": "#",
+			"class": "event-success",
+			"start": "1450496896789",
+			"end":   "1450496896989"
+		},
+		{
+			"id": "294",
+			"title": "迟到不好",
+			"url": "#",
+			"class": "event-warning",
+			"start": "1450496896789",
+			"end":   "1450496896989"
+		},
+		{
+			"id": "256",
+			"title": "早上不要迟到咯",
+			"url": "#",
+			"class": "event-info",
+			"start": "1450396896789",
+			"end":   "1450396896889"
+		},
+		{
+			"id": "276",
+			"title": "Short day event",
+			"url": "http://www.example.com/",
+			"class": "event-success",
+			"start": "1363245600000",
+			"end":   "1363252200000"
+		},
+		{
+			"id": "294",
+			"title": "This is information class ",
+			"url": "#",
+			"class": "event-info",
+			"start": "1363111200000",
+			"end":   "1363284086400"
+		},
+		{
+			"id": "297",
+			"title": "This is success event",
+			"url": "http://www.example.com/",
+			"class": "event-success",
+			"start": "1363234500000",
+			"end":   "1363284062400"
+		},
+		{
+			"id": "54",
+			"title": "This is simple event",
+			"url": "#",
+			"class": "",
+			"start": "1363712400000",
+			"end":   "1363716086400"
+		},
+		{
+			"id": "532",
+			"title": "This is inverse event",
+			"url": "http://www.example.com/",
+			"class": "event-inverse",
+			"start": "1364407200000",
+			"end":   "1364493686400"
+		},
+		{
+			"id": "548",
+			"title": "This is special event",
+			"url": "http://www.example.com/",
+			"class": "event-special",
+			"start": "1363197600000",
+			"end":   "1363629686400"
+		},
+		{
+			"id": "295",
+			"title": "Event 3",
+			"url": "http://www.example.com/",
+			"class": "event-important",
+			"start": "1364320800000",
+			"end":   "1364407286400"
+		}
+	]
+}
