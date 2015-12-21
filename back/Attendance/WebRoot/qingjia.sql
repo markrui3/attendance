53a46cf2734b7eb74c71697b530a86bfa364ec37
董春江 <990194688@qq.com>
2015-12-21 22:45:11 +0800

    数据库

--- back/Attendance/WebRoot/qingjia.sql
+++ back/Attendance/WebRoot/qingjia.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2015-12-06 16:59:20
+Date: 2015-12-21 22:43:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -90,8 +90,9 @@ DROP TABLE IF EXISTS `sign`;
 CREATE TABLE `sign` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `userid` bigint(20) NOT NULL,
-  `timecome` datetime NOT NULL,
-  `timeleave` datetime NOT NULL,
+  `timecome` int(11) NOT NULL,
+  `timeleave` int(11) NOT NULL,
+  `time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -113,13 +114,14 @@ CREATE TABLE `user` (
   `birthday` date NOT NULL,
   `position` varchar(50) NOT NULL,
   `startworkdate` date NOT NULL,
+  `email` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '13920742173', '技术部', 'dcj', '董春江', '0', '1991-06-24', '0', '2013-06-24');
-INSERT INTO `user` VALUES ('2', '1234', '技术部', 'zr', '赵瑞', '0', '1993-12-11', '1', '2012-07-04');
-INSERT INTO `user` VALUES ('3', '1122', '技术部', 'dyq', '董一强', '0', '1993-04-06', '2', '2012-07-09');
-INSERT INTO `user` VALUES ('4', '2211', '技术部', 'xhl', '谢杭伦', '1', '1993-01-01', '3', '2011-06-06');
+INSERT INTO `user` VALUES ('1', '13920742173', '技术部', 'dcj', '董春江', '0', '1991-06-24', '0', '2013-06-24', '');
+INSERT INTO `user` VALUES ('2', '1234', '技术部', 'zr', '赵瑞', '0', '1993-12-11', '1', '2012-07-04', '');
+INSERT INTO `user` VALUES ('3', '1122', '技术部', 'dyq', '董一强', '0', '1993-04-06', '2', '2012-07-09', '');
+INSERT INTO `user` VALUES ('4', '2211', '技术部', 'xhl', '谢杭伦', '1', '1993-01-01', '3', '2011-06-06', '');
