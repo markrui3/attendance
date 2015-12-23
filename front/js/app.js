dea17a5cea838661e2c86e0db6c313daeff0790b
markrui <87541708@qq.com>
2015-12-24 00:08:09 +0800

    前台接口对接

--- front/js/app.js
+++ front/js/app.js
@@ -10,6 +10,13 @@ app.controller('ctrl_homepage', function($scope, $http){
 		$scope.pm25 = response.results[0].pm25;
 		$scope.des = response.results[0].index[0].des;
 	});
+	$http
+	.get('http://api.map.baidu.com/telematics/v3/weather?location=%E5%A4%A9%E6%B4%A5&output=json&ak=A070e14d4e685d26358a908a3c896567')
+	.success(function (response){
+		$scope.temperature = response.results[0].weather_data[0].temperature;
+		$scope.pm25 = response.results[0].pm25;
+		$scope.des = response.results[0].index[0].des;
+	});
 	$scope.comeSign = function(){
 		mui.toast('开始');
 	};
