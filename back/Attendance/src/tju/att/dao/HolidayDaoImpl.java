25c43c68a7b9137fdb2e3a8fe66887b2a765a766
董春江 <990194688@qq.com>
2015-12-27 21:49:53 +0800

    update lastday num

--- back/Attendance/src/tju/att/dao/HolidayDaoImpl.java
+++ back/Attendance/src/tju/att/dao/HolidayDaoImpl.java
@@ -10,9 +10,13 @@ public class HolidayDaoImpl extends BaseDaoImpl<Holiday> implements HolidayDao{
 	@Override
 	public List<Object[]> getSumDay(Long userid) {
 		String sql = "select a.holidayid as id, sum(a.lastday) as days from Attendance a "
-				+ "WHERE a.userid = ? GROUP BY a.holidayid";
+				+ "WHERE a.userid = ? AND a.status <> ? AND a.status <> ? AND a.status <> ?"
+				+ "  GROUP BY a.holidayid";
 		return getSession().createSQLQuery(sql)
 				.setParameter(0, userid)
+				.setParameter(1, REFUSECHECK_1)
+				.setParameter(2, REFUSECHECK_2)
+				.setParameter(3, REFUSECHECK_3)
 				.list();
 	}
 
